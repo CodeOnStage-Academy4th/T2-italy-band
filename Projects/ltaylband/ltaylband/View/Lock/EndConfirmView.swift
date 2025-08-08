72db0f8f1f6b620ec933dff9c30ec800adf18bbf
허찬욱 <101908750+hkwangro@users.noreply.github.com>
2025-08-09 07:40:10 +0900

    design: 디자인 변경 사항 및 주석 제거
    디자인 변경 사항 및 주석 제거

--- Projects/ltaylband/ltaylband/View/Lock/EndConfirmView.swift
+++ Projects/ltaylband/ltaylband/View/Lock/EndConfirmView.swift
@@ -1,20 +1,5 @@
-//
-//  EndConfirmView.swift
-//  ltaylband
-//
-//  Created by 광로 on 12/20/24.
-//
 
 import SwiftUI
-
-// 디버깅: 색상 테스트
-private func testColors() {
-    print("🎨 색상 테스트:")
-    print("  - ColorSet.rock100: \(ColorSet.rock100)")
-    print("  - Color(\"Rook100\"): \(Color("Rook100"))")
-    print("  - 직접 RGB: \(Color(red: 0.702, green: 0.792, blue: 0.161))")
-    print("  - Color.gray: \(Color.gray)")
-}
 
 struct EndConfirmView: View {
     let onContinue: () -> Void
@@ -32,19 +17,18 @@ struct EndConfirmView: View {
                     Text("집중을 종료하시겠습니까?")
                         .jejudoldamFont(size: ._20, weight: .regular)
                         .foregroundColor(ColorSet.black80)
-
+                    
                     VStack(spacing: 8) {
                         Text("돌이 더 춤추고 싶어해요!")
                             .jejudoldamFont(size: ._14, weight: .regular)
                             .foregroundColor(ColorSet.black60)
-
+                        
                         Text("돌을 슬프게 하지 말아주세요...")
                             .jejudoldamFont(size: ._14, weight: .regular)
                             .foregroundColor(ColorSet.black60)
                     }
                 }
                 HStack(spacing: 16) {
-                    // 이어하기 버튼 (흰색 배경, 검은색 텍스트, 회색 테두리)
                     Button(action: onContinue) {
                         Text("이어하기")
                             .jejudoldamFont(size: ._16, weight: .regular)
@@ -58,8 +42,6 @@ struct EndConfirmView: View {
                             )
                             .cornerRadius(12)
                     }
-                    
-                    // 종료하기 버튼 (회색 배경, 흰색 텍스트)
                     Button(action: onEnd) {
                         Text("종료하기")
                             .jejudoldamFont(size: ._16, weight: .regular)
@@ -75,9 +57,6 @@ struct EndConfirmView: View {
             .background(Color.white)
             .cornerRadius(16)
             .padding(.horizontal, 32)
-        }
-        .onAppear {
-            testColors()
         }
     }
 }
