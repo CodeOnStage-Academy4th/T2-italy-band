116f1b8df21348010a369347b5486a6784667484
김재윤/Eddey <jaeyoon7421@naver.com>
2025-08-09 01:09:28 +0900

    feat: 네비게이션스택 및 잠금화면  캐릭터 모션 설정

--- Projects/ltaylband/ltaylband/View/Main/Home/HomeView.swift
+++ Projects/ltaylband/ltaylband/View/Main/Home/HomeView.swift
@@ -10,14 +10,23 @@ import SwiftUI
 //TODO: 타이머와 잠금버튼 함께 구현
 //TODO: RockView 여기에 들어가야 함.
 struct HomeView: View {
+    @EnvironmentObject var router: AppRouter
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            // LockView로 이동하는 버튼
+            Button("Lock View로 이동") {
+                router.navigate(to: .lock)
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
+        .navigationTitle("Home")
     }
 }
 
