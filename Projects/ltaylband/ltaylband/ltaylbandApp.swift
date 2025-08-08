cc316e39686a4ec64ceb8bfc76f73558164ee915
김재윤/Eddey <jaeyoon7421@naver.com>
2025-08-08 19:48:14 +0900

    feat: 초기 프로젝트 설정

---
+++ Projects/ltaylband/ltaylband/ltaylbandApp.swift
@@ -0,0 +1,17 @@
+//
+//  ltaylbandApp.swift
+//  ltaylband
+//
+//  Created by 김재윤 on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ltaylbandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
