505f9efaba34127b8afcd5ac2efb201dd7ba654a
김재윤/Eddey <jaeyoon7421@naver.com>
2025-08-08 21:19:01 +0900

    feat: 모델링 작업 완료

---
+++ Projects/ltaylband/ltaylband/Model/Grade.swift
@@ -0,0 +1,49 @@
+//
+//  Grade.swift
+//  ltaylband
+//
+//  Created by 김재윤 on 8/8/25.
+//
+
+import Foundation
+
+
+enum Grade: String, CaseIterable, Codable {
+    case joyakdol = "조약돌"
+    case hawgangam = "화강암"
+    case jasujeong = "자수정"
+    case emerald = "에메랄드"
+    case diamond = "다이아몬드"
+    
+}
+
+extension Grade {
+    
+    //TODO: 시간 기준 정해야함.
+    static func from(spentTime: Int) -> Grade {
+        switch spentTime {
+        case 0..<100:
+            return .joyakdol
+        case 100..<200:
+            return .hawgangam
+        case 200..<300:
+            return .jasujeong
+        case 300..<400:
+            return .emerald
+        case 400..<500:
+            return .diamond
+        default:
+            return .joyakdol
+        }
+    }
+    
+    var backgroundImageName: String {
+        switch self {
+        case .joyakdol: return "bg_joyakdol"
+        case .hawgangam: return "bg_hawgangam"
+        case .jasujeong: return "bg_jasujeong"
+        case .emerald: return "bg_emerald"
+        case .diamond: return "bg_diamond"
+        }
+    }
+}
