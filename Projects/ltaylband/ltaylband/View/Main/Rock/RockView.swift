c9f740937aaf4abbd3e5ca37e8c50f8cbd34c521
김재윤/Eddey <jaeyoon7421@naver.com>
2025-08-08 22:08:33 +0900

    feat: 프로젝트 폴더 구조 1차본

---
+++ Projects/ltaylband/ltaylband/View/Main/Rock/RockView.swift
@@ -0,0 +1,19 @@
+//
+//  RockView.swift
+//  ltaylband
+//
+//  Created by 김재윤 on 8/8/25.
+//
+
+import SwiftUI
+
+//TODO: 이미지 또는 움직이는 돌멩이 누르면 커스텀뷰로 이동하게끔 구현
+struct RockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RockView()
+}
